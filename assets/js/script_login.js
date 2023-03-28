10fcc5f5aeff1b886e1f038352e87fb5734993ca
Daniel <danielmtdc16@gmail.com>
2023-03-28 12:38:34 -0300

    Trocando icon por img nos cards.

---
+++ assets/js/script_login.js
@@ -0,0 +1,9 @@
+let professor = document.getElementById('img-teacher');
+let employee = document.querySelector('.img-employee');
+let employeeStyle = window.getComputedStyle(employee);
+let cardTeacher = document.getElementById('card-teacher');
+let cardEmployee = document.querySelector('.card-employee');
+let cardEmployeeStyle = window.getComputedStyle(cardEmployee);
+
+
+
